ff88a090efb1ce498ac679e208765b961f85277c
Nicholas <nicholas_1022@hotmail.com>
2022-05-29 00:05:08 +0800

    modified to test CI of Jenkins

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World! This messaged is modified to test CI of Jenkins";
 
     public App() {}
 
